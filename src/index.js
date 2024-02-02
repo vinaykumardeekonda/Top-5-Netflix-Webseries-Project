4bb4083616ed9ac7c866c87cb1abffc3b22cde9b
vinay-kumar-projects <vinayprojects@gmail.com>
2024-02-02 14:41:52 +0530

    Top 5 netflix Webseries list Created Using ReactJS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <>
+    <h1>Vinay Kumar Netflix Pick</h1>
+    <h4>List of Top 5 series: </h4>
+    <ol>
+        <li>Dark</li>
+        <li>Extra Curricular</li>
+        <li>My Holo Love</li>
+        <li>My First-2 Love</li>
+        <li>Mr. Robot</li>
+    </ol>
+    </>,
+    document.getElementById("root"));
